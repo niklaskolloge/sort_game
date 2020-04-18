d48fbbb3717208a0b27679b0c807969931014ea0
Niklas Kolloge <niklas.kolloge@nordakademie.de>
2020-04-18 21:33:32 +0200

    add all files

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DragDropModule,
+    NoopAnimationsModule,
+  ],
+  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
